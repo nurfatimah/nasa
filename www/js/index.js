afa9e32b225c2e24b926b0261c60d070f6ed0d7d
nurfatimah <48873225+nurfatimah@users.noreply.github.com>
2019-04-08 13:33:02 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=yxdZUrQ8fwX3OxmQcEJZTdVBxp7Sy6pRuwXshfjw";
         
         $("#loadImage").click(function(){
             $.ajax({
